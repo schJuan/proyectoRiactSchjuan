8586321681a0cd6198652ba195d26329198586e1
schlotthauerjuan <juan_41629456gmail.com>
2023-11-28 22:10:12 -0300

    primer repo riact

---
+++ node_modules/hiper-product-card/dist/components/index.d.ts
@@ -0,0 +1,6 @@
+import { ProductCardHOCProps } from "../interfaces/interfaces";
+export { ProductButtons } from "./ProductButtons";
+export { ProductImage } from "./ProductImage";
+export { ProductTitle } from "./ProductTitle";
+export declare const ProductCard: ProductCardHOCProps;
+export default ProductCard;
